0294d4bf79b6985b86d06319bb5cbb0e12eb0dae
martinacerboni <acerboni_martin@hotmail.com>
2018-11-24 01:24:53 -0300

    streams y pipe

--- nodejs/projectoWeb/core.js
+++ nodejs/projectoWeb/core.js
@@ -1,10 +1,15 @@
 var http= require('http');
 var events= require('events');
+var dataBase= require('./dataBase.js');
+var fs = require('fs');
+var url= require('url');
 
 var myEmitter= new events.EventEmitter();
 var sumar= function(a,b){
   return a + b;
 }
+
+console.log(dataBase.string_multiplicar(2,3));
 
 myEmitter.on('unEvento',function(mssg,int){
   console.log(mssg);
@@ -15,10 +20,14 @@ myEmitter.on('unEvento',function(mssg,int){
 
 
 http.createServer(function(req,res){
-  console.log('hola guacho');
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.write('Hello World! \n');
+  var texto= url.parse(req.url,true);
+  console.log(texto.search);
+  res.writeHead(200, {'Content-Type': 'text/plain'});
   res.write(sumar(4,6).toString());
   myEmitter.emit('unEvento','unMesnajeLoco', 8);
-  res.end();
+
+  //esto deberia leer del req y escribirlo en res
+  var readableStream = fs.createReadStream(__dirname + '/unTextoLargo.txt','utf8');
+  readableStream.pipe(res);
+  //res.end(); si esto no esta comentado, el pipe no llega a escribir en la response
 }).listen(3000);
---
+++ nodejs/projectoWeb/dataBase.js
@@ -0,0 +1,5 @@
+var string_multiplicar= function(a,b){
+  return (a*b).toString();
+};
+
+module.exports = {string_multiplicar: string_multiplicar}
